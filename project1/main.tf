9fa95f257e9d507423e045b452d7fc78b4bdd474
sm1 <sm1>
2019-09-05 10:39:34 +0000

    vm and network 2

--- project1/main.tf
+++ project1/main.tf
@@ -4,6 +4,56 @@
 ##
 #####################################################################
 
+## REFERENCE {"vsphere_network":{"type": "vsphere_reference_network"}}
+
 terraform {
   required_version = "> 0.8.0"
+}
+
+provider "vsphere" {
+  user           = "${var.user}"
+  password       = "${var.password}"
+  vsphere_server = "${var.vsphere_server}"
+
+  allow_unverified_ssl = "${var.allow_unverified_ssl}"
+  version = "~> 1.2"
+}
+
+
+data "vsphere_virtual_machine" "test1_template" {
+  name          = "${var.test1_template_name}"
+  datacenter_id = "${data.vsphere_datacenter.test1_datacenter.id}"
+}
+
+data "vsphere_datacenter" "test1_datacenter" {
+  name = "${var.test1_datacenter_name}"
+}
+
+data "vsphere_datastore" "test1_datastore" {
+  name          = "${var.test1_datastore_name}"
+  datacenter_id = "${data.vsphere_datacenter.test1_datacenter.id}"
+}
+
+data "vsphere_network" "network1" {
+  name          = "${var.network1_network_name}"
+  datacenter_id = "${data.vsphere_datacenter.test1_datacenter.id}"
+}
+
+resource "vsphere_virtual_machine" "test1" {
+  name          = "${var.test1_name}"
+  datastore_id  = "${data.vsphere_datastore.test1_datastore.id}"
+  num_cpus      = "${var.test1_number_of_vcpu}"
+  memory        = "${var.test1_memory}"
+  guest_id = "${data.vsphere_virtual_machine.test1_template.guest_id}"
+  resource_pool_id = "${var.test1_resource_pool}"
+  network_interface {
+    network_id = "${data.vsphere_network.network1.id}"
+  }
+  clone {
+    template_uuid = "${data.vsphere_virtual_machine.test1_template.id}"
+  }
+  disk {
+    name = "${var.test1_disk_name}"
+    size = "${var.test1_disk_size}"
+  }
 }
--- project1/variables.tf
+++ project1/variables.tf
@@ -4,3 +4,73 @@
 ##
 #####################################################################
 
+variable "user" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "password" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "vsphere_server" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "allow_unverified_ssl" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "test1_name" {
+  type = "string"
+  description = "Virtual machine name for test1"
+}
+
+variable "test1_number_of_vcpu" {
+  type = "string"
+  description = "Number of virtual cpu's."
+}
+
+variable "test1_memory" {
+  type = "string"
+  description = "Memory allocation."
+}
+
+variable "test1_disk_name" {
+  type = "string"
+  description = "The name of the disk. Forces a new disk if changed. This should only be a longer path if attaching an external disk."
+}
+
+variable "test1_disk_size" {
+  type = "string"
+  description = "The size of the disk, in GiB."
+}
+
+variable "test1_template_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "test1_datacenter_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "test1_datastore_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "test1_resource_pool" {
+  type = "string"
+  description = "Resource pool."
+}
+
+variable "network1_network_name" {
+  type = "string"
+  description = "Generated"
+}
+
